d67416dcd527001a35e6f64529a546413975a7a5
smileypop <matthewlaird@gmail.com>
2023-05-28 16:22:14 +0900

    remove deploymentBranch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sahha-ai', // Usually your GitHub org/user name.
   projectName: 'sahha-developer-site', // Usually your repo name.
-  deploymentBranch: 'master',
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
