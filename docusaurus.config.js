af58dd2fb116390ec95677aaf1b8bc5e14fa70a2
smileypop <matthewlaird@gmail.com>
2023-05-29 17:00:44 +0900

    update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,7 @@ const config = {
   url: 'https://developer.sahha.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sahha-developer-site/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
